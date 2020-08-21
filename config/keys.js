3a82b88367cbfedbe96c671d51d66d1014ac3136
adignwn <broken.wayan@gmail.com>
2020-08-21 15:28:28 +0800

    fix config

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: "mongodb+srv://admin:<password>@cluster0.xpngn.mongodb.net/<dbname>?retryWrites=true&w=majority",
+        mongoURI: "mongodb+srv://admin:amin123@cluster0.xpngn.mongodb.net/app4?retryWrites=true&w=majority",
         secret: 'yoursecret'
     }
 } else {
