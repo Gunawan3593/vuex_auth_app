1a2546b81276e086c11db94d2606378c38b7d281
adignwn <broken.wayan@gmail.com>
2020-08-21 14:41:34 +0800

    production Database added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,11 @@
-module.exports = {
-    mongoURI: "mongodb://localhost:27017/app3",
-    secret: 'yoursecret'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = {
+        mongoURI: "mongodb://admin:admin123@cluster0.xpngn.mongodb.net/app4",
+        secret: 'yoursecret'
+    }
+} else {
+    module.exports = {
+        mongoURI: "mongodb://localhost:27017/app3",
+        secret: 'yoursecret'
+    }
 }
