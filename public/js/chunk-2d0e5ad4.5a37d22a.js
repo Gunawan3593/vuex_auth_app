8d6e390f6d8ce2c2a78b6f8d8959516b02251e0e
adignwn <broken.wayan@gmail.com>
2020-09-12 14:26:27 +0800

    deployment

--- public/js/chunk-2d0e5ad4.5a37d22a.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5ad4"],{9601:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("404 Page not found")])])}],r={},u=r,o=t("2877"),s=Object(o["a"])(u,a,c,!1,null,"4afe9a36",null);n["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0e5ad4.5a37d22a.js.map
