8f002f68ea6ed61a76fa9f1a5a937d806cf0c377
Adi Gunawan <broken.wayan@gmail.com>
2020-09-20 12:24:54 +0800

    deployment

---
+++ public/js/chunk-2d0e5ad4.3aa7ec20.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5ad4"],{9601:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("404 Page not found")])])}],u={},a=u,o=t("2877"),s=Object(o["a"])(a,c,r,!1,null,"791772bf",null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0e5ad4.3aa7ec20.js.map
